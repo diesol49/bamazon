1d696260d801357069122aa3b7eb8e7b7cfd76e1
unknown <diesol42@gmail.com>
2019-06-03 17:06:14 -0700

    setting up the files

---
+++ schema.sql
@@ -0,0 +1,17 @@
+-- DROP THE DATABASE IF IT ALREADY EXISTS --
+DROP DATABASE IF EXISTS bamazon_db;
+
+-- CREATE OUR bamazon_db DATABASE --
+CREATE DATABASE bamazon_db;
+
+-- SELECT OUR DATABASE TO USE --
+USE bamazon_db;
+-- WE THEN CREATE OUT TABLE --
+CREATE TABLE products (
+    item_id INT(10) NOT NULL,
+    product_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
+    price INT(10),
+    stock_quantity INT(10) NOT NULL
+    PRIMARY KEY (item_id)
+    );
